cc9ceabe4f9c63dd4719d3d06d6d2deff32ee695
Leodas <leodasplaysmc@gmail.com>
2025-10-18 12:57:46 +0530

    Create mine.sh

---
+++ mine.sh
@@ -0,0 +1,5 @@
+apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y && git clone https://github.com/xmrig/xmrig.git
+
+mkdir xmrig/build && cd xmrig/build
+
+cmake .. && make -j$(nproc)
