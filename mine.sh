bea48d871f4493c248aa2a06e536c25ea17ec998
Leodas <leodasplaysmc@gmail.com>
2025-10-18 12:58:35 +0530

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -3,3 +3,5 @@ apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y 
 mkdir xmrig/build && cd xmrig/build
 
 cmake .. && make -j$(nproc)
+
+cd xmrig/build && ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u DOGE:D7hFQES9Tr6ipvmZEHfoHkVTEcCHmu4oUU.unmineable_worker_xhwopbyl#qdew-l30g -p x  --threads=8
